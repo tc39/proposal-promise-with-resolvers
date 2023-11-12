e33d10e871b5089d602f232bb27a8ecf0c857c77
John Henry <john@iamjohnhenry.com>
2023-11-12 08:47:34 -0800

    Update polyfills.js
    to return an iterable object as to support array-deconstructing

--- polyfills.js
+++ polyfills.js
@@ -1,6 +1,12 @@
 export function withResolvers() {
 	if (!this) throw new TypeError("Promise.withResolvers called on non-object")
-	const out = {}
+	const out = {
+		*[Symbol.iterator](){
+     			yield this.promise;
+     			yield this.resolve;
+     			yield this.reject;
+  		}
+	}
 	out.promise = new this((resolve_, reject_) => {
 		out.resolve = resolve_
 		out.reject = reject_
